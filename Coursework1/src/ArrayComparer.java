2b79c33fe964589e8c23154f6cc2b59cddc98d0c
jackscotti <jacopojackscotti@gmail.com>
2014-10-01 12:25:07 +0100

    Allow continued user input

--- Coursework1/src/ArrayComparer.java
+++ Coursework1/src/ArrayComparer.java
@@ -4,29 +4,29 @@ public class ArrayComparer {
 
   static int[] array1 = new int[100];
   static int counter = 0;
+  static boolean stopInput = false;
 //  static int[] array2 = new int[100];
 
   static Scanner input = new Scanner( System.in );
 
   public static void main(String[] args) {
-	  addNumberToArray(number());
-	  addNumberToArray(number());
-	  addNumberToArray(number());
-	  
+ 
+	  while (stopInput == false && counter < 5){
+		  addNumberToArray(number());
+	  }
 
 	  for (int element : array1) {
        System.out.println(element);
     }
   }
-  
+ 
   // get user input
   public static int number(){
 	  int number;
 	  System.out.println("Add integer, type 0 if you want to terminate");
 	  number = input.nextInt();
 	  return number;
-  }
-	  
+  }  
   public static void addNumberToArray(int number) {
 	  array1[counter] = number;
 	  counter++;
