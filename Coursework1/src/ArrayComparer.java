d321728230fd2cfc7869d25fcdd8e81e44910ce1
jackscotti <jacopojackscotti@gmail.com>
2014-10-01 12:17:19 +0100

    Accept user input

--- Coursework1/src/ArrayComparer.java
+++ Coursework1/src/ArrayComparer.java
@@ -9,17 +9,26 @@ public class ArrayComparer {
   static Scanner input = new Scanner( System.in );
 
   public static void main(String[] args) {
-	  addNumberToArray(1);
-	  addNumberToArray(9);
-	  addNumberToArray(23);
+	  addNumberToArray(number());
+	  addNumberToArray(number());
+	  addNumberToArray(number());
+	  
 
 	  for (int element : array1) {
        System.out.println(element);
-	  }
+    }
+  }
+  
+  // get user input
+  public static int number(){
+	  int number;
+	  System.out.println("Add integer, type 0 if you want to terminate");
+	  number = input.nextInt();
+	  return number;
   }
 	  
   public static void addNumberToArray(int number) {
-		  array1[counter] = number;
-		  counter++;
-	  }
+	  array1[counter] = number;
+	  counter++;
+  	}
   }
