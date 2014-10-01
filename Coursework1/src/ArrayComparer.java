30d27d962e43349036dc2652c6c169425c2f92b4
jackscotti <jacopojackscotti@gmail.com>
2014-10-01 12:08:12 +0100

    Add addNumberToArray method

--- Coursework1/src/ArrayComparer.java
+++ Coursework1/src/ArrayComparer.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 
 public class ArrayComparer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+  static int[] array1 = new int[100];
+  static int counter = 0;
+//  static int[] array2 = new int[100];
 
-	}
+  static Scanner input = new Scanner( System.in );
 
-}
+  public static void main(String[] args) {
+	  addNumberToArray(1);
+	  addNumberToArray(9);
+	  addNumberToArray(23);
+
+	  for (int element : array1) {
+       System.out.println(element);
+	  }
+  }
+	  
+  public static void addNumberToArray(int number) {
+		  array1[counter] = number;
+		  counter++;
+	  }
+  }
